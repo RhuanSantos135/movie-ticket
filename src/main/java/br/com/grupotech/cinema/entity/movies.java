5e54cd1d3d8c8b77c8c8e60847f786d525cf0ae0
yuri amaral santos <113852509+Yuri-amaralsantos@users.noreply.github.com>
2024-12-05 21:57:23 -0300

    subindo test

---
+++ src/main/java/br/com/grupotech/cinema/entity/movies.java
@@ -0,0 +1,14 @@
+package br.com.grupotech.cinema.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class movies {
+    private Long id;
+    private String title;
+}
